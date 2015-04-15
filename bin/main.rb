34caf2e84fc563ee4550c63ee73852b1f79c373c
Steffan Voges <steffan@berkeley.edu>
2015-04-15 15:42:13 +1200

    started template for iterations over generations

--- bin/main.rb
+++ bin/main.rb
@@ -10,7 +10,16 @@ else
   world = World.new(ARGV[0].to_i, ARGV[1].to_i)
 end
 
-# Create creatures. population could be done better
 world.initialize_world
 
-world.display_world
+generations = 10
+
+while generations > 0
+  time_step = 10
+  while time_step > 0
+    world.iterate
+    world.display_world
+    time_step -=1
+  end
+  generations -=1
+end
