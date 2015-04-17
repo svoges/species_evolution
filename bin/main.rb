d681d9c2a75669dc16b2c15982583aa847ae0106
Steffan Voges <steffan@berkeley.edu>
2015-04-18 10:46:45 +1200

    help option included

--- bin/main.rb
+++ bin/main.rb
@@ -4,24 +4,28 @@ require_relative '../lib/world.rb'
 
 
 if ARGV.length < 2
-  puts "ERROR: Need 2 inputs for x and y values of array"
-  exit
+  if ARGV.include?('-help')
+    puts "Help information for Evolve a Species Project"
+    puts "Usage: ruby [x_location] [y_location] [options]"
+    puts "   -i      enable manual printing after each iteration"
+    puts "   -m      enable manual printing after each movement"
+    exit
+  else
+    puts "ERROR: Need 2 inputs for x and y values of array"
+    exit
+  end
 elsif ARGV[0].to_i > 0 and ARGV[1].to_i > 0
   # if true, allows the user to step through each creature movement
   manual_movement = false
   # if true, allows the user to step through each world iteration
   manual_iteration = false
-  if ARGV.include?('-help')
-    puts "OPTION HERE"
-  else
-    if ARGV.include?('-i')
-      manual_iteration = true
-    end
-    if ARGV.include?('-m')
-      manual_movement = true
-    end
-    world = World.new(ARGV[0].to_i, ARGV[1].to_i, manual_movement, manual_iteration)
+  if ARGV.include?('-i')
+    manual_iteration = true
   end
+  if ARGV.include?('-m')
+    manual_movement = true
+  end
+  world = World.new(ARGV[0].to_i, ARGV[1].to_i, manual_movement, manual_iteration)
 end
 
 # Create creatures
