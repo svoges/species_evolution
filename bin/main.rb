aa0a01f9f98e87ad9c91f374c3d1f410daab0d57
Steffan Voges <steffan@berkeley.edu>
2015-05-08 20:47:35 +1200

    teacher mode implemented for presentation

--- bin/main.rb
+++ bin/main.rb
@@ -6,6 +6,7 @@ if ARGV.length < 2
   if ARGV.include?('-help')
     puts "Help information for Evolve a Species Project"
     puts "Usage: ruby [x_location] [y_location] [options]"
+    puts "   -t      enables teacher mode, where I give the presentation"
     puts "   -v      enable more detailed printing"
     puts "   -i      enable manual printing after each iteration, 'exit' or 'quit' to abort"
     puts "   -m      enable manual printing after each movement, 'exit' or 'quit' to abort"
@@ -23,9 +24,13 @@ elsif ARGV[0].to_i > 0 and ARGV[1].to_i > 0
   # if true, allows the user to step through each world iteration
   manual_iteration = false
   # if true, enables generational mode in which evolution will occur
+  teacher_mode = false
   generations = false
   if ARGV.include?('-v')
     printing = true
+  end
+  if ARGV.include?('-t')
+    teacher_mode = true
   end
   if ARGV.include?('-i')
     manual_iteration = true
@@ -40,6 +45,20 @@ elsif ARGV[0].to_i > 0 and ARGV[1].to_i > 0
   world = World.new(ARGV[0].to_i, ARGV[1].to_i, printing, manual_movement, manual_iteration)
 end
 world = World.new(ARGV[0].to_i, ARGV[1].to_i, printing, manual_movement, manual_iteration)
+
+if teacher_mode
+  # do presentation
+  puts "Initial population behavior"
+  initial_world = World.new(ARGV[0].to_i, ARGV[1].to_i, true, manual_movement, false)
+  initial_world.populate
+  input = ""
+  while input != "quit" and input != "exit"
+    initial_world.do_iteration
+    puts "Press enter to continue"
+    STDOUT.flush
+    input = STDIN.gets.chomp
+  end
+end
 
 if generations
   File.truncate('output/average.txt', 0)
@@ -63,5 +82,16 @@ else
   world.populate
   while !world.get_persons.empty?
     world.do_iteration
+  end
+end
+
+if teacher_mode
+  puts "Behavior after evolution"
+  world.create_generation
+  world.populate
+  world.enable_manual_iteration
+  while true
+    world.do_iteration
+    iterations -= 1
   end
 end
